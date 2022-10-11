85cbb6df35f6b6c198e627101caf3cc2f37aecac
Dawid Koruba <dko@leapwork.com>
2022-10-11 10:15:17 +0200

    working version of diamond printer

---
+++ DiamondPrinter/Printer.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace DiamondPrinter;
+
+public static class Printer
+{
+    private static readonly string EmptySpace = " ";
+    public static string PrintDiamond(char mainLetter)
+    {
+        var lines = GetTopHalfOfDiamond(mainLetter);
+
+        var linesToMirror = lines.Take(lines.Count - 1);
+        var mirroredLines = linesToMirror.Reverse();
+        lines.AddRange(mirroredLines);
+
+        return string.Join(Environment.NewLine, lines);
+    }
+
+    private static List<string> GetTopHalfOfDiamond(char mainLetter)
+    {
+        var distanceFromA = (mainLetter - 'A');
+        var diamondWidth = (int) distanceFromA * 2 + 1;
+
+        var lines = new List<string>();
+
+        for (var row = 0; row <= distanceFromA; row++)
+        {
+            lines.Add(PrintLine(mainLetter, row, diamondWidth));
+        }
+
+        return lines;
+    }
+
+    private static string PrintLine(char mainLetter, int row, int count)
+    {
+        var lineBuilder = new StringBuilder();
+        var letter = (char) ('A' + row);
+        for (var column = 0; column < count; column++)
+        {
+            var character = ShouldLetterBeInColumn(letter, mainLetter, column) ? $"{letter}" : EmptySpace; 
+            lineBuilder.Append(character);
+        }
+
+        return lineBuilder.ToString();
+    }
+
+    public static bool ShouldLetterBeInColumn(char letter, char mainLetter, int column)
+    {
+        var mainLetterDistanceFromA = mainLetter - 'A';
+        var letterDistanceFromA = letter - 'A';
+        var distanceFromLeftBorder = mainLetterDistanceFromA - letterDistanceFromA;
+        var width = mainLetterDistanceFromA * 2 + 1;
+        var distanceFromRightBorder = width - 1 - distanceFromLeftBorder;
+        return column == distanceFromLeftBorder || column == distanceFromRightBorder;
+    }
+}
---
+++ DiamondPrinter/Tests.cs
@@ -0,0 +1,94 @@
+﻿using NUnit.Framework;
+
+namespace DiamondPrinter;
+
+public class Tests
+{
+    [Test]
+    public void Given_A_Then_Print_A()
+    {
+        var expected = "A";
+        Assert.AreEqual(expected, Printer.PrintDiamond('A'));
+    }
+
+    [Test]
+    public void Given_B_Then_Print_ABB()
+    {
+        var expected =
+            " A \r\n" +
+            "B B\r\n" +
+            " A ";
+        Assert.AreEqual(expected, Printer.PrintDiamond('B'));
+    }
+
+    [Test]
+    public void Given_C_Then_Print_ABBCC()
+    {
+        var expected =
+            "  A  \r\n" +
+            " B B \r\n" +
+            "C   C\r\n" +
+            " B B \r\n" +
+            "  A  ";
+        Assert.AreEqual(expected, Printer.PrintDiamond('C'));
+    }
+
+    [Test]
+    public void Given_F_Then_Print_ABBCCDDEEFF()
+    {
+        var expected =
+            "     A     \r\n" +
+            "    B B    \r\n" +
+            "   C   C   \r\n" +
+            "  D     D  \r\n" +
+            " E       E \r\n" +
+            "F         F\r\n" +
+            " E       E \r\n" +
+            "  D     D  \r\n" +
+            "   C   C   \r\n" +
+            "    B B    \r\n" +
+            "     A     ";
+
+        Assert.AreEqual(expected, Printer.PrintDiamond('F'));
+    }
+
+    [Test]
+    public void GetLetterPositions_in_A_diamond()
+    {
+        Assert.IsTrue(Printer.ShouldLetterBeInColumn('A', 'A', 0));
+    }
+
+    [Test]
+    public void GetLetterPositions_in_B_diamond_Then_3()
+    {
+        Assert.IsFalse(Printer.ShouldLetterBeInColumn('A', 'B', 0));
+        Assert.IsTrue(Printer.ShouldLetterBeInColumn('A', 'B', 1));
+        Assert.IsFalse(Printer.ShouldLetterBeInColumn('A', 'B', 2));
+
+        Assert.IsTrue(Printer.ShouldLetterBeInColumn('B', 'B', 0));
+        Assert.IsFalse(Printer.ShouldLetterBeInColumn('B', 'B', 1));
+        Assert.IsTrue(Printer.ShouldLetterBeInColumn('B', 'B', 2));
+    }
+
+    [Test]
+    public void GetLetterPositions_in_C_diamond_Then_3()
+    {
+        Assert.IsFalse(Printer.ShouldLetterBeInColumn('A', 'C', 0));
+        Assert.IsFalse(Printer.ShouldLetterBeInColumn('A', 'C', 1));
+        Assert.IsTrue(Printer.ShouldLetterBeInColumn('A', 'C', 2));
+        Assert.IsFalse(Printer.ShouldLetterBeInColumn('A', 'C', 3));
+        Assert.IsFalse(Printer.ShouldLetterBeInColumn('A', 'C', 4));
+
+        Assert.IsFalse(Printer.ShouldLetterBeInColumn('B', 'C', 0));
+        Assert.IsTrue(Printer.ShouldLetterBeInColumn('B', 'C', 1));
+        Assert.IsFalse(Printer.ShouldLetterBeInColumn('B', 'C', 2));
+        Assert.IsTrue(Printer.ShouldLetterBeInColumn('B', 'C', 3));
+        Assert.IsFalse(Printer.ShouldLetterBeInColumn('B', 'C', 4));
+
+        Assert.IsTrue(Printer.ShouldLetterBeInColumn('C', 'C', 0));
+        Assert.IsFalse(Printer.ShouldLetterBeInColumn('C', 'C', 1));
+        Assert.IsFalse(Printer.ShouldLetterBeInColumn('C', 'C', 2));
+        Assert.IsFalse(Printer.ShouldLetterBeInColumn('C', 'C', 3));
+        Assert.IsTrue(Printer.ShouldLetterBeInColumn('C', 'C', 4));
+    }
+}
